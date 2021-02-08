f5e4ac4080be79b397070b67eee011545ef66489
CerritoCode0101 <CaseyCerrito@gmail.com>
2021-02-08 16:58:27 -0600

    MVP!!

--- api/server.js
+++ api/server.js
@@ -1,3 +1,71 @@
+const model = require("./users/model");
+
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express");
+
+
+const app = express();
+app.use(express.json());
+
+app.get("/api/users", (req, res) => {
+  model
+    .find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => res.status(500).json({ message: `Server Error :${err.message}` }));
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const idVar = req.params.id;
+  model
+    .findById(idVar)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ message: `ID ${idVar} does not exist.` });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch((err) => res.status(500).json({ message: `Server Error :${err.message}` }));
+});
+
+app.post("/api/users",(req,res)=>{
+    const {name, bio} = req.body;
+    model.insert({name,bio}).then((newUser)=>{
+       res.status(201).json(newUser)
+}).catch((err)=>{
+    console.log(err.message)
+})
+});
+
+
+
+app.put("/api/users/:id", (req,res)=>{
+    const idVar =req.params.id
+    const user= req.body
+    model.update(idVar,user).then((upUser)=>{
+        if (upUser === null){
+            res.status(404).json({message:" User does not exist."})
+        }else {
+            res.status(200).json(upUser)
+        }
+        
+    }).catch(err => {
+        console.log(err.message)
+    })
+})
+
+app.delete("/api/users/:id", (req,res)=>{
+    const idVar = req.params.id;
+    model.remove(idVar).then((user)=>{
+        res.status(200).json({message: `The user at ID: ${user.id} was deleted`})
+    }).catch((err)=>{ console.log(err.message)})
+})
+
+    app.use("*",(req,res)=>{
+        res.status(404).json({message:"404 not found)*"})
+    })
+
+module.exports = app;
--- api/users/model.js
+++ api/users/model.js
@@ -29,7 +29,7 @@ const insert = ({ name, bio }) => {
   // INSERT INTO users (name, bio) VALUES ('foo', 'bar');
   const newUser = { id: shortid.generate(), name, bio }
   users.push(newUser)
-  return Promise.resolve(newUser)
+  return Promise.resolve(find())
 }
 
 const update = (id, changes) => {
